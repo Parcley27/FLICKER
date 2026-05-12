661abb302c30892238a70e5a12f6e40c5d3be614
Pierce Oxley <95258151+Parcley27@users.noreply.github.com>
2026-05-12 12:30:52 -0700

    Tensor init and types

---
+++ pytorch practice/practice.py
@@ -0,0 +1,30 @@
+# From https://docs.pytorch.org/tutorials/beginner/blitz/tensor_tutorial.html
+
+import torch
+import numpy as np
+
+# Tensor init
+
+data = [[1, 2], [3, 4]]
+xData = torch.tensor(data)
+
+npArray = np.array(data)
+xNp = torch.from_numpy(npArray)
+
+print(xData)
+print(xNp)
+
+xOnes = torch.ones_like(xData) # retains the properties of xData
+print(f"Ones Tensor: \n {xOnes} \n")
+
+xRand = torch.rand_like(xData, dtype=torch.float) # overrides the datatype of xData
+print(f"Random Tensor: \n {xRand} \n")
+
+shape = (2, 3)
+randomTensor = torch.rand(shape)
+onesTensor = torch.ones(shape)
+zerosTensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {randomTensor} \n")
+print(f"Ones Tensor: \n {onesTensor} \n")
+print(f"Zeros Tensor: \n {zerosTensor}")
